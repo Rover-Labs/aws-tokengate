b6078836d1066790c7217d64d50ac469677131b0
Xavier <business.xsantos@gmail.com>
2022-05-07 18:32:28 -0700

    added db connection

--- index.js
+++ index.js
@@ -1,35 +1,89 @@
 const Moralis = require("moralis/node");
+const Analytics = require("analytics-node");
+const mysql = require("mysql");
 
-exports.handler = async (event) => {
-  const address = event["params"]["querystring"]["address"];
-  const nftAddress = event["params"]["querystring"]["nftAddress"];
-  const chain = event["params"]["querystring"]["chain"];
-  const serverUrl = "https://ckb11ejzq8dp.grandmoralis.com:2053/server";
-  const appId = "lDDXYagREnknCf9WxDm994rB9YcHiEfk767ehbna";
-  const moralisSecret =
-    "1gSzI1YNfLvgwZjTD2l9y6nq6elWKwtUinHBKGuhTLDU1NoKbASJu1dFpOeSjnJp";
-  await Moralis.start({ serverUrl, appId, moralisSecret });
+const analytics = new Analytics("DAsmuBOK66rejbtN9vCfHW3zFs7rA3yj");
 
-  if (chain === "solana") {
-    const options = {
-      chain: "mainnet",
-      address: address,
-    };
-    const nftBalance = await Moralis.SolanaAPI.account.getNFTs(options);
-    return nftBalance;
-  } else {
-    const options = {
-      chain: chain,
-      address: address,
-      token_address: nftAddress,
-    };
-    const polygonNFTs = await Moralis.Web3API.account.getNFTsForContract(
-      options
-    );
-    if (polygonNFTs.total >= 1) {
-      return true;
-    } else {
-      return false;
-    }
+const db = mysql.createConnection({
+  host: "tokengatewhite.cmnoqeobfm8g.us-east-2.rds.amazonaws.com",
+  port: "3306",
+  user: "Watson",
+  password: "rE2Yf7A2l1Y$Dvuw",
+  database: "sys",
+});
+
+db.connect((err) => {
+  if (err) {
+    console.log(err.message);
+    return;
   }
-};
+  console.log("Database connected.");
+})
+
+// exports.handler = async (event) => {
+
+//   // User Address Parameter
+//   const address = event["params"]["querystring"]["address"];
+//   // Collection Address Parameter
+//   const nftAddress = event["params"]["querystring"]["nftAddress"];
+//   // Chain Parameter
+//   const chain = event["params"]["querystring"]["chain"];
+
+//   // Moralis API Initialization
+//   const serverUrl = "https://ckb11ejzq8dp.grandmoralis.com:2053/server";
+//   const appId = "lDDXYagREnknCf9WxDm994rB9YcHiEfk767ehbna";
+//   const moralisSecret =
+//     "1gSzI1YNfLvgwZjTD2l9y6nq6elWKwtUinHBKGuhTLDU1NoKbASJu1dFpOeSjnJp";
+//   await Moralis.start({ serverUrl, appId, moralisSecret });
+
+//   analytics.track({
+//     userId: address,
+//     event: "Token Gate Used",
+//     properties: {
+//       chain: chain,
+//       nftAddress: nftAddress,
+//     },
+//   });
+
+//   // Solana Token Gating (Does not work yet)
+//   if (chain === "solana") {
+//     const options = {
+//       chain: "mainnet",
+//       address: address,
+//     };
+//     const nftBalance = await Moralis.SolanaAPI.account.getNFTs(options);
+//     return nftBalance;
+
+//   // Ethereum Token Gating
+//   } else {
+//     const options = {
+//       chain: chain,
+//       address: address,
+//       token_address: nftAddress,
+//     };
+//     const polygonNFTs = await Moralis.Web3API.account.getNFTsForContract(
+//       options
+//     );
+//     if (polygonNFTs.total >= 1) {
+//       analytics.track({
+//         userId: address,
+//         event: "Token Gate Success",
+//         properties: {
+//           chain: chain,
+//           nftAddress: nftAddress,
+//         },
+//       });
+//       return true;
+//     } else {
+//       analytics.track({
+//         userId: address,
+//         event: "Token Gate Failed",
+//         properties: {
+//           chain: chain,
+//           nftAddress: nftAddress,
+//         },
+//       });
+//       return false;
+//     }
+//   }
+// };
