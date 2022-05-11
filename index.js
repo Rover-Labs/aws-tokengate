6484a0038f1a4de18e75a2059f5a33bf9e8586b2
Xavier <business.xsantos@gmail.com>
2022-05-10 20:54:48 -0700

    Corrected API endpoint

--- index.js
+++ index.js
@@ -96,7 +96,7 @@ exports.handler = async (event) => {
 
     try {
       let res = await axios.get(
-        `https://api.opensea.io/api/v1/asset_contract/${contractAddress}`,
+        `https://api.opensea.io/api/v1/assets?owner=${address}&collection={$nftAddress}`,
         {
           headers: {
             "X-API-KEY": process.env.XAPIKEY,
